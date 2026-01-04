6bd683b43285d2581f91fadf42dad48249949f6d
dmsandhika <dmsandhika87@gmail.com>
2026-01-04 23:53:48 +0700

    Refactor database connection to use PostgreSQL: update connection string and driver in InitDB function

--- internal/config/database.go
+++ internal/config/database.go
@@ -2,20 +2,21 @@ package config
 
 import (
 	"os"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	dsn := os.Getenv("DB_USER") + ":" +
-		os.Getenv("DB_PASS") + "@tcp(" +
-		os.Getenv("DB_HOST") + ":" +
-		os.Getenv("DB_PORT") + ")/" +
-		os.Getenv("DB_NAME") + "?parseTime=true"
+  dsn := "host=" + os.Getenv("DB_HOST") +
+	       " user=" + os.Getenv("DB_USER") +
+	       " password=" + os.Getenv("DB_PASS") +
+	       " dbname=" + os.Getenv("DB_NAME") +
+	       " port=" + os.Getenv("DB_PORT") +
+	       " sslmode=disable TimeZone=Asia/Jakarta"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect database")
-	}
-	return db
+       db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+       if err != nil {
+	       panic("Failed to connect database")
+       }
+       return db
 }
