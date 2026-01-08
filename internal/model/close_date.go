1190d64d161fa4b2447807f7d8d9da72c7c0c032
dmsandhika <dmsandhika87@gmail.com>
2026-01-09 00:08:32 +0700

    Update CloseDate struct to use GORM tags for database mapping

--- internal/model/close_date.go
+++ internal/model/close_date.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type CloseDate struct {
-	ID        int64     `db:"id"`
-	Date      time.Time `db:"date"`
-	Reason    string    `db:"reason"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID        int64     `gorm:"primaryKey" json:"id"`
+	Date      time.Time `gorm:"column:date" json:"date"`
+	Reason    string    `gorm:"column:reason" json:"reason"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
