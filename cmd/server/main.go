53e7de2556f4865d5b59e800670a0da09dba6960
dmsandhika <dmsandhika87@gmail.com>
2026-01-09 00:18:13 +0700

    Refactor database migration to handle models individually and improve error logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 
 	"futsal-booking/internal/config"
 	"futsal-booking/internal/handler"
@@ -18,9 +19,24 @@ func main() {
 	godotenv.Load()
 
 	db := config.InitDB()
-	if err := db.AutoMigrate(&model.Court{}, &model.Admin{}, &model.Booking{}, &model.CloseDate{}); err != nil {
-		log.Fatal("Failed to migrate database:", err)
+	
+	// Migrate models dengan option untuk skip table yang sudah ada
+	db = db.Session(&gorm.Session{SkipDefaultTransaction: true})
+	
+	if err := db.AutoMigrate(&model.Court{}); err != nil {
+		log.Println("Failed to migrate Court:", err)
 	}
+	if err := db.AutoMigrate(&model.Admin{}); err != nil {
+		log.Println("Failed to migrate Admin:", err)
+	}
+	if err := db.AutoMigrate(&model.Booking{}); err != nil {
+		log.Println("Failed to migrate Booking:", err)
+	}
+	if err := db.AutoMigrate(&model.CloseDate{}); err != nil {
+		log.Println("Failed to migrate CloseDate:", err)
+	}
+	
+	log.Println("All migrations completed")
 
 	courtRepo := &repository.CourtRepository{DB: db}
 	courtHandler := &handler.CourtHandler{Repo: courtRepo}
